54472cff1d9f40b491e8724e218588ccdc274314
StevieTV <stevie.robinson@gmail.com>
2026-03-22 17:14:40 +0100

    add connection to rabbitmq

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,25 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+
+  const connectionString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(connectionString);
+
+  console.log("Connected to RabbitMQ Server");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await conn.close();
+        console.log("RabbitMQ connection closed.");
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 }
 
 main().catch((err) => {
